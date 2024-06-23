816296df7ad3787f47c5a76e666fdcff26a2a4bf
Emil Karlen <emil.karlen@fripost.org>
2024-06-23 15:13:19 +0200

    ADD CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use clap::Parser;
+
+/// Read file names from stdin (one per line) and rename the file so that its extension is lowercase
+#[derive(Parser)]
+struct Cli
+{
+    /// Do execute the action (default is to run dry)
+    #[arg(short)]
+    x: bool,
+}
+
+fn main()
+{
+    let args = Cli::parse();
+
+    println!("mode: {:?}", args.x);
+
     println!("Hello, world!");
 }
